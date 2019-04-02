08cbf44ac0656ffd622065259ca17bd927c66e3a
adam mckenzie <adammckenzie@adams-MacBook-Pro-2.local>
2019-04-02 14:52:33 -0500

    initial

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -3,6 +3,8 @@ import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
+import axios from "axios";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -48,15 +50,15 @@ class App extends Component {
     }
   }
   render() {
-    const { products, showCart } = this.state;
+    const { products, showCart, cart } = this.state;
     return (
       <div className="App">
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} />
+            <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
           ) : (
-            <StoreFront products={products} />
+            <StoreFront products={products} addToCart={this.addToCart} />
           )}
         </div>
       </div>
